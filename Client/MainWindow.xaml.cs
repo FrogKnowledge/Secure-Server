75cfac47302f06691eb9722aa88ade1dbcfd2570
Stankevich Vlad <memopad258@gmail.com>
2020-04-29 17:54:02 +0300

    Загрузка изначального проекта

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Security;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int port = 8005; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+
+        }
+      /*  public void Send(object sender,EventArgs args)
+        {
+            try
+            {
+                IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(address), port);
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                // подключаемся к удаленному хосту
+                socket.Connect(ipPoint);
+                byte[] data = Encoding.Unicode.GetBytes(message.Text);
+                socket.Send(data);
+
+                // получаем ответ
+                data = new byte[256]; // буфер для ответа
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0; // количество полученных байт
+
+                do
+                {
+                    bytes = socket.Receive(data, data.Length, 0);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (socket.Available > 0);
+                message.Text="ответ сервера: " + builder.ToString();
+
+                // закрываем сокет
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Console.Read();
+        }*/
+    }
+}
